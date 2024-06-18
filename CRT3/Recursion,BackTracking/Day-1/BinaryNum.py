814beed4b30607479bb731241d98dd61704c1982
Bhargavi Bogadi <116951997+21A31A0502@users.noreply.github.com>
2024-06-18 17:01:55 +0530

    Delete CRT3/Recursion,BackTracking/Day-1 directory

--- CRT3/Recursion,BackTracking/Day-1/BinaryNum.py
+++
@@ -1,8 +0,0 @@
-def printBinaryNum(size,res,n):
-    if size==n:
-        print(res)
-        return 
-    printBinaryNum(size+1,res+'1',n)
-    printBinaryNum(size+1,res+'0',n)
-n=4
-printBinaryNum(0,'',n)    
